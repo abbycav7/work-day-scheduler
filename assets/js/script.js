fe2fec1e44bf7ccd24ff3d6ee41a18472cf089d4
Abigail <asexton5@nd.edu>
2021-04-21 13:58:29 -0400

    added in get tasks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,38 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+// tasks object to store in localStorage.
+var tasks = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+};
+
+var setTasks = function() {
+    /* add tasks to localStorage */
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+var getTasks = function() {
+    /* load the tasks from localStorage and create tasks in the right row */
+
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadedTasks) {
+        tasks = loadedTasks
+
+        // for each key/value pair in tasks, create a task
+        $.each(tasks, function(hour, task) {
+            var hourDiv = $("#" + hour);
+            createTask(task, hourDiv);
+        })
+    }
+
+    // make sure the past/current/future time is reflected
+    auditTasks()
+}
